d589e7da0f5c4260602817241a7912717c3595ac
ChloeArd <chloe.ardoise@gmail.com>
2020-12-10 11:00:48 +0100

    Correction morpion

--- app.js
+++ app.js
@@ -5,22 +5,25 @@ let cases = document.getElementsByClassName("case");
 
 
 //Ici square contient un nouvel élément .case de la liste d'éléments a chaque passage de la boucle !
+//Maintenant que j'ai tous mes éléments, je dois trouver une solution pour savoir QUI et SI on a cliqué.
 for(const square of cases) {
 
+    //Comment trouver quel bouton a été cliqué pour savoir qui a joué.
     // On ajoute un listener sur chaque élément de classe .case.
     square.addEventListener('mouseup', function(event) {
         switch(event.button) {
             case CLICK_LEFT:
-                // Faire en sorte d'afficher un X pour le gauche, mai ne pas oublier.
+                // Faire en sorte d'afficher un X pour le gauche.
                 //Trouver la meilleur méthode: <img>? backgroundImage ? valeur texte ????
+                this.innerHTML = "X"; //Contient l'objet qui a généré l'évènement (l'objet sur lequel on a clic).
 
                 //Une fois que le X sera dans le div, on ne pourra plus rien ajouter dans ce div.
 
                 break;
 
             case CLICK_RIGHT:
-                //Faire en sorte d'afficher un O pour le droit, mai ne pas oublier .
-
+                //Faire en sorte d'afficher un O pour le droit.
+                this.innerHTML = "O";
                 //Une fois que le O sera dans le div, on ne pourra plus rien ajouter dans ce div.
                 break;
         }
