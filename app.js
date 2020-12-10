5f309df5f9ba02a9983acb651c5d6c994b0b8344
ChloeArd <chloe.ardoise@gmail.com>
2020-12-10 13:30:10 +0100

    Correction morpion

--- app.js
+++ app.js
@@ -30,21 +30,84 @@ for(const square of cases) {
 
 // Vérifier quelque chose.
 function checkCases() {
-    //Un vérification horizontale.
-    //doit tester 0, 1, 2
-    //            3, 4, 5
-    //            6, 7, 8
+    let result = checkHorizontal();
+    //Si el résultat est autre chose qu'une chaine (donc false) alors on passe au test suivant !
+    if(!result) { // revient à ==> result === false
+        result = checkVertical();
+    }
 
-    //Une vérification verticale.
-    //doit tester 0, 3, 6
-    //            1, 4, 7
-    //            2, 5, 8
 
-    //Une verification diagonale.
+    checkDiagonal();
+}
+
+//Un vérification horizontale.
+function checkHorizontal() {
+    //On traite la 1ère ligne ==> 0, 1, 2
+   /** if (cases[0].innerHTML === "X" && cases[1].innerHTML === "X" && cases[2].innerHTML === "X") {
+        return 'Player 1 won !'
+    }
+
+    else if (cases[0].innerHTML === "O" && cases[1].innerHTML === "O" && cases[2].innerHTML === "O") {
+        return 'Player 2 won !'
+    }
+
+    //On traite la 2è ligne ==> 3, 4, 5
+    if (cases[3].innerHTML === "X" && cases[4].innerHTML === "X" && cases[5].innerHTML === "X") {
+        return 'Player 1 won !'
+    }
+
+    else if (cases[3].innerHTML === "O" && cases[4].innerHTML === "O" && cases[5].innerHTML === "O") {
+        return 'Player 2 won !'
+    }
+
+    //On traite la 3è ligne ==> 6, 7, 8
+    if (cases[6].innerHTML === "X" && cases[7].innerHTML === "X" && cases[8].innerHTML === "X") {
+        return 'Player 1 won !'
+    }
+
+    else if (cases[6].innerHTML === "O" && cases[7].innerHTML === "O" && cases[8].innerHTML === "O") {
+        return 'Player 2 won !'
+    }
+    */
+
+    // Le code juste au dessus en commentaire revient au code juste en dessous en plus simplifié !
+    for (let index = 0; index <= 8; index += 3) { //On a fait un pas de 3 a chaque fois de 0 à 3, de 1 à 4, ...
+        //Et la au 1er passage, j'aurai 0, 1, 2 / au 2è j'aurai 3, 4, 5 et au 3è j'aurai 6, 7, 8
+        if (cases[index].innerHTML === "X" && cases[index+1].innerHTML === "X" && cases[index+2].innerHTML === "X") {
+            return 'Player 1 won !'
+        }
+
+        else if (cases[index].innerHTML === "O" && cases[index+1].innerHTML === "O" && cases[index+2].innerHTML === "O") {
+            return 'Player 2 won !'
+        }
+    }
+    return false;
+}
+
+//Une vérification verticale.
+function checkVertical() {
+    for (let index = 0; index <= 2; index ++) { //On a fait un pas de 3 a chaque fois de 0 à 3, de 1 à 4, ...
+        //Et la au 1er passage, j'aurai 0, 3, 6 / au 2è j'aurai 1, 4, 7 et au 3è j'aurai 2, 5, 8
+        if (cases[index].innerHTML === "X" && cases[index+3].innerHTML === "X" && cases[index+6].innerHTML === "X") {
+            return 'Player 1 won !'
+        }
+
+        else if (cases[index].innerHTML === "O" && cases[index+3].innerHTML === "O" && cases[index+6].innerHTML === "O") {
+            return 'Player 2 won !'
+        }
+    }
+    return false;
+
+}
+
+//Une verification diagonale.
+function checkDiagonal() {
     //doit tester 0, 4, 8
     //            2, 4, 6
 
 }
+
+
 
 
 
