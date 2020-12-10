095303033d424736627154aea71101062e624dd6
ChloeArd <chloe.ardoise@gmail.com>
2020-12-10 11:42:56 +0100

    Correction morpion

--- app.js
+++ app.js
@@ -7,35 +7,61 @@ let cases = document.getElementsByClassName("case");
 //Ici square contient un nouvel élément .case de la liste d'éléments a chaque passage de la boucle !
 //Maintenant que j'ai tous mes éléments, je dois trouver une solution pour savoir QUI et SI on a cliqué.
 for(const square of cases) {
-
-    //Comment trouver quel bouton a été cliqué pour savoir qui a joué.
     // On ajoute un listener sur chaque élément de classe .case.
     square.addEventListener('mouseup', function(event) {
         switch(event.button) {
             case CLICK_LEFT:
-                // Faire en sorte d'afficher un X pour le gauche.
-                //Trouver la meilleur méthode: <img>? backgroundImage ? valeur texte ????
-                this.innerHTML = "X"; //Contient l'objet qui a généré l'évènement (l'objet sur lequel on a clic).
-
                 //Une fois que le X sera dans le div, on ne pourra plus rien ajouter dans ce div.
-
+               insertPlayerText(this, "X")
                 break;
 
             case CLICK_RIGHT:
-                //Faire en sorte d'afficher un O pour le droit.
-                this.innerHTML = "O";
                 //Une fois que le O sera dans le div, on ne pourra plus rien ajouter dans ce div.
+               insertPlayerText(this, "O")
                 break;
         }
 
         //toujours après avoir effectué un clic, vérifier si qlqn a gagné, function???
+        checkCases();
 
-
-    });
-
-    // Empêche un élément par défault de se produire, ici on empèche que le menu apparaisse quand on fait un clique droit qu'au niveau du square !!!!
-    square.addEventListener("contextmenu", function (event) {
-        event.preventDefault();
     });
 }
+
+
+// Vérifier quelque chose.
+function checkCases() {
+    //Un vérification horizontale.
+    //doit tester 0, 1, 2
+    //            3, 4, 5
+    //            6, 7, 8
+
+    //Une vérification verticale.
+    //doit tester 0, 3, 6
+    //            1, 4, 7
+    //            2, 5, 8
+
+    //Une verification diagonale.
+    //doit tester 0, 4, 8
+    //            2, 4, 6
+
+}
+
+
+
+
+
+
+//Insère une lettre dans le div .case
+function insertPlayerText(element, playerChar) {
+    //Element contient l'élément a qui on va donner la lettre contenue dans playerChar.
+    if(element.innerHTML.length === 0) {
+        //PlayerChar contient la lettre à insérer, X ou O
+        element.innerHTML = playerChar; //Contient l'objet qui a généré l'évènement (l'objet sur lequel on a clic)
+    }
+}
+
+// Empêche un élément par défault de se produire, ici on empèche que le menu apparaisse quand on fait un clique droit qu'au niveau du square !!!!
+document.addEventListener("contextmenu", function (event) {
+    event.preventDefault();
+});
 
