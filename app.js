4c51cc568d5c23c2c54476e78f19098d91060244
ChloeArd <chloe.ardoise@gmail.com>
2020-12-10 10:47:44 +0100

    Correction morpion

--- app.js
+++ app.js
@@ -5,18 +5,28 @@ let cases = document.getElementsByClassName("case");
 
 
 //Ici square contient un nouvel élément .case de la liste d'éléments a chaque passage de la boucle !
-for(let square of cases) {
+for(const square of cases) {
 
     // On ajoute un listener sur chaque élément de classe .case.
     square.addEventListener('mouseup', function(event) {
         switch(event.button) {
             case CLICK_LEFT:
-                console.log("click gauche");
+                // Faire en sorte d'afficher un X pour le gauche, mai ne pas oublier.
+                //Trouver la meilleur méthode: <img>? backgroundImage ? valeur texte ????
+
+                //Une fois que le X sera dans le div, on ne pourra plus rien ajouter dans ce div.
+
                 break;
 
             case CLICK_RIGHT:
-                console.log("click droit");
+                //Faire en sorte d'afficher un O pour le droit, mai ne pas oublier .
+
+                //Une fois que le O sera dans le div, on ne pourra plus rien ajouter dans ce div.
                 break;
         }
+
+        //toujours après avoir effectué un clic, vérifier si qlqn a gagné, function???
+
+
     });
 }
